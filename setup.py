b86ed620c39359948fee72314bc4e31e037fff0e
Shilpa Jha <shilpajmd123@gmail.com>
2025-07-29 17:51:01 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'medicalbot',
+    version = '0.0.0',
+    author = 'Shilpa',
+    author_email='shilpajmd123@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
